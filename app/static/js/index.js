dc21c35f173c2f730bedcbbbe5d4c7b78cfd8858
greendata <edy.eggenberger@gmail.com>
2024-03-28 19:08:31 +0100

    testing routing ig?

---
+++ app/static/js/index.js
@@ -0,0 +1,48 @@
+import Home from "./views/Home.js";
+
+const navigateTo = url => {
+    history.pushState(null, null, url);
+    router();
+};
+
+const router = async () => {
+    const routes = [
+        { path: "/", view: () => Home },
+        // { path: "/home", view: () => console.log("Viewing home!") },
+        // { path: "/post", view: () => console.log("Viewing posts!") },
+    ];
+
+    const potentialMatches = routes.map(route => {
+        return {
+            route: route,
+            isMatch: location.pathname === route.path
+        };
+    });
+
+    let match = potentialMatches.find(potentialMatch => potentialMatch.isMatch);
+
+    // handle unknown paths
+    if (!match){
+        match = {
+            route: { path: "", view: () => console.log("404") },
+            isMatch: true
+        };
+    };
+
+    const view = new match.route.view();
+
+    document.querySelector("#app").innerHTML = await view.getHtml();
+}
+
+window.addEventListener("popstate", router);
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.addEventListener("click", e => {
+        if (e.target.matches("[data-link]")) {
+            e.preventDefault();
+            navigateTo(e.target.href);
+        }
+    });
+
+    router();
+});
